ace1ac3c4c06714665f2ea9d7f5a89fd4e7785bb
syed <user.email>
2022-08-11 19:59:03 +0100

    added basic layout with input setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import { greet } from "./utils/greet";
+import "./App.css";
+import { useState } from "react";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [inputText, setInputText] = useState("");
+
+  const handleOnEnter = (key: string): void => {
+    if (key === "Enter") {
+      console.log("entered!");
+    }
+  };
+
+  const handleInputChange = (input: string): void => {
+    setInputText(input)
+  }
+
+  return (
+    <>
+      <h1>TODO APP</h1>
+      <input
+        className="addTodoClass"
+        value={inputText}
+        onKeyPress={(ev) => handleOnEnter(ev.key)}
+        onChange={(ev) => handleInputChange(ev.target.value)}
+        placeholder="Create a new todo..."
+      ></input>
+      <main>
+        {/* divs collected here from database in REST API stored in Heroku */}
+        <div className="todoListClass"> - go gorceries </div> <div className="editClass">EDIT     </div><div className="deleteClass"> DELETE</div>
+        <div className="todoListClass"> - clean laundry </div> <div className="editClass">EDIT </div><div className="deleteClass"> DELETE</div>
+        <div className="todoListClass"> - buy charger </div> <div className="editClass">EDIT </div><div className="deleteClass"> DELETE</div>
+      </main>
+    </>
+  );
 }
 
 export default App;
