f464a2b7afc58064951caa9d78ebc26d4a46d16f
syed <user.email>
2022-08-12 14:31:09 +0100

    added todo component plus dummy axios commands

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import "./App.css";
 import { useState } from "react";
+import axios from "axios";
+
+// function updateTodoDB() {
+//   axios.post("https://todo-list-syed.herokuapp.com/items/",{"todo":"go shopping!"})
+//     .then((res) => console.log(res));
+// }
+
+// updateTodoDB()
 
 function App(): JSX.Element {
   const [inputText, setInputText] = useState("");
+  const [text, setText] = useState("")
 
   const handleOnEnter = (key: string): void => {
     if (key === "Enter") {
       console.log("entered!");
+      //send current input to backend
+      axios.get("https://todo-list-syed.herokuapp.com/items/")
+    .then((res) => {
+      console.log(res);
+      setText(res.data[7].todo)
+    });
     }
   };
 
   const handleInputChange = (input: string): void => {
-    setInputText(input)
-  }
+    setInputText(input);
+  };
 
   return (
     <>
@@ -25,10 +40,17 @@ function App(): JSX.Element {
         placeholder="Create a new todo..."
       ></input>
       <main>
-        {/* divs collected here from database in REST API stored in Heroku */}
-        <div className="todoListClass"> - go gorceries </div> <div className="editClass">EDIT     </div><div className="deleteClass"> DELETE</div>
-        <div className="todoListClass"> - clean laundry </div> <div className="editClass">EDIT </div><div className="deleteClass"> DELETE</div>
-        <div className="todoListClass"> - buy charger </div> <div className="editClass">EDIT </div><div className="deleteClass"> DELETE</div>
+        {/* divs collected here from database in REST API stored in Heroku DB */}
+        {text}
+        <div className="todoListClass"> - go gorceries </div>{" "}
+        <div className="editClass">EDIT </div>
+        <div className="deleteClass"> DELETE</div>
+        <div className="todoListClass"> - clean laundry </div>{" "}
+        <div className="editClass">EDIT </div>
+        <div className="deleteClass"> DELETE</div>
+        <div className="todoListClass"> - buy charger </div>{" "}
+        <div className="editClass">EDIT </div>
+        <div className="deleteClass"> DELETE</div>
       </main>
     </>
   );
