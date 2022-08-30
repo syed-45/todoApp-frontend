3763961028bc03041b80f6d8deac62195ca2d2ef
syed <user.email>
2022-08-30 17:34:05 +0100

    working delete

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,19 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import TodoList from "./Components/todoList";
 
+interface todoData {
+  todo: string;
+  id: number;
+  checked: boolean;
+}
+
 function App(): JSX.Element {
   const [inputText, setInputText] = useState("");
-  const [allTodos, setAllTodos] = useState<JSX.Element[]>([]);
+  const [allTodos, setAllTodos] = useState<todoData[]>([]);
 
   useEffect(() => {
     axios.get("https://todo-list-syed.herokuapp.com/items/").then((res) => {
-      console.log(res);
-      setAllTodos(
-        res.data.map(
-          (oneData: { todo: string; id: number; checked: boolean }) => (
-            <TodoList
-              todo={oneData.todo}
-              id={oneData.id}
-              key={oneData.id}
-              onClick={handleDeleteClick}
-              checked={oneData.checked}
-            />
-          )
-        )
-      );
+      setAllTodos(res.data);
     });
   }, []);
 
@@ -33,18 +26,7 @@ function App(): JSX.Element {
           todo: inputText,
           checked: false,
         })
-        .then((res) =>
-          setAllTodos([
-            ...allTodos,
-            <TodoList
-              todo={res.data.todo}
-              id={res.data.id}
-              key={res.data.id}
-              onClick={handleDeleteClick}
-              checked={false}
-            />,
-          ])
-        );
+        .then((res) => setAllTodos([...allTodos, res.data]));
       setInputText("");
     }
   };
@@ -54,16 +36,11 @@ function App(): JSX.Element {
   };
 
   const handleDeleteClick = (id: number): void => {
-    const currentTodos = [...allTodos];
-    const removeIndex = currentTodos.findIndex((todo) => {
-      if (todo.key) {
-        return parseInt(todo.key.toString()) === id;
-      }
-      return false;
-    });
-    // currentTodos.splice(removeIndex);
-    // setAllTodos(currentTodos)
-    console.log(removeIndex);
+    console.log(id);
+
+    axios
+      .delete(`https://todo-list-syed.herokuapp.com/items/${id}`)
+      .then(() => setAllTodos(allTodos.filter((oneData) => oneData.id !== id)));
   };
 
   return (
@@ -76,7 +53,17 @@ function App(): JSX.Element {
         onChange={(ev) => handleInputChange(ev.target.value)}
         placeholder="Create a new todo..."
       ></input>
-      <main>{allTodos}</main>
+      <main>
+        {allTodos.map((oneData) => (
+          <TodoList
+            todo={oneData.todo}
+            id={oneData.id}
+            key={oneData.id}
+            onClick={handleDeleteClick}
+            checked={oneData.checked}
+          />
+        ))}
+      </main>
     </>
   );
 }
